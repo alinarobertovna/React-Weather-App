3f73a3c9c358e4e7f025dcc06f2561d1f228ed81
Alina <alinarobertovna17.03@gmail.com>
2023-12-13 23:06:43 -0500

    Fixed time conversion for different locations

--- src/components/CityWeather.js
+++ src/components/CityWeather.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { WiDaySunny, WiRain, WiStrongWind, WiHumidity, WiBarometer } from 'react-icons/wi';
-import { BsSearch } from 'react-icons/bs';
+import { WiRain, WiStrongWind, WiHumidity, WiMoonAltNew } from 'react-icons/wi';
+import { BsSearch, BsGeoAlt } from 'react-icons/bs';
 
 const CityWeather = ({ cityData, handleSubmit, isCelsius, toggleTempUnit }) => {
   const [cityInput, setCityInput] = useState('');
@@ -13,6 +13,41 @@ const CityWeather = ({ cityData, handleSubmit, isCelsius, toggleTempUnit }) => {
     e.preventDefault();
     handleSubmit(cityInput);
   };
+
+  const getCurrentDateTime = (timezoneOffsetInSeconds) => {
+    if (typeof timezoneOffsetInSeconds !== 'number') {
+      throw new Error('timezoneOffsetInSeconds must be a number representing the offset in seconds');
+    }
+  
+    // Create a new Date object for the current time
+    const now = new Date();
+  
+    // Calculate local time in UTC
+    const utc = now.getTime() + (now.getTimezoneOffset() * 60000);
+  
+    // Create a new Date object for the local time in the given timezone
+    const localTime = new Date(utc + (timezoneOffsetInSeconds * 1000));
+  
+    // Format the date and time
+    const day = localTime.getDate();
+    const monthIndex = localTime.getMonth();
+    const year = localTime.getFullYear();
+    const hours = localTime.getHours();
+    const minutes = localTime.getMinutes();
+  
+    // Add leading zero to minutes and hours if necessary
+    const formattedHours = hours < 10 ? `0${hours}` : hours;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  
+    // Array of month names
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  
+    // Get the month name
+    const monthName = monthNames[monthIndex];
+  
+    return `Date: ${day} ${monthName} ${year} <br /> Time: ${formattedHours}:${formattedMinutes}`;
+  };
+  
 
   return (
     <div className="relative bg-white bg-opacity-80 rounded-lg shadow-lg overflow-hidden w-full p-4 lg:max-w-3xl lg:mx-auto">
@@ -40,10 +75,10 @@ const CityWeather = ({ cityData, handleSubmit, isCelsius, toggleTempUnit }) => {
         {/* Location row with temperature unit toggle */}
         <div className="flex justify-between items-center mb-4">
           <div className="flex items-center">
-            <WiDaySunny className="text-yellow-400 text-3xl mr-3" />
-            <span className="font-semibold text-lg md:text-2xl">{cityData.name}, {cityData.sys.country}</span>
+            <BsGeoAlt className="text-yellow-400 text-3xl mr-3" />
+            <span className="font-semibold text-lg md:text-2xl">{cityData.name}, {cityData.sys.country}</span> 
           </div>
-          <button onClick={toggleTempUnit} className="text-gray-600 text-xs md:text-sm">
+          <button onClick={toggleTempUnit} className="text-gray-600 font-semibold text-lg md:text-2xl">
             {isCelsius ? '°F' : '°C'}
           </button>
         </div>
@@ -52,7 +87,9 @@ const CityWeather = ({ cityData, handleSubmit, isCelsius, toggleTempUnit }) => {
         <div className="my-4 md:my-6 text-center">
           <span className="text-gray-800 font-bold text-4xl md:text-6xl lg:text-9xl">{Math.round(cityData.main.temp)}</span>
           <span className="text-gray-800 font-semibold text-xl md:text-2xl lg:text-4xl">{isCelsius ? '°C' : '°F'}</span>
-          <p className="text-gray-600 text-sm md:text-md lg:text-lg">Today</p>
+          <div className="text-gray-600 text-sm md:text-md lg:text-lg">
+            Feels like: {Math.round(cityData.main.feels_like)}{isCelsius ? '°C' : '°F'}
+          </div>
         </div>
 
         {/* Additional weather details row */}
@@ -64,16 +101,16 @@ const CityWeather = ({ cityData, handleSubmit, isCelsius, toggleTempUnit }) => {
           </div>
           <div className="flex flex-col items-center">
             <WiStrongWind className="text-teal-400 text-3xl md:text-4xl" />
-            <span className="text-xs md:text-sm mt-2">Wind: {cityData.wind.speed} {isCelsius ? 'm/s' : 'mph'}</span>
+            <span className="text-xs md:text-sm mt-2">Wind: {Math.round(cityData.wind.speed)} {isCelsius ? 'm/s' : 'mph'}</span>
           </div>
           <div className="flex flex-col items-center">
             <WiHumidity className="text-blue-400 text-3xl md:text-4xl" />
             <span className="text-xs md:text-sm mt-2">Humidity: {cityData.main.humidity}%</span>
           </div>
           <div className="flex flex-col items-center">
-            <WiBarometer className="text-purple-400 text-3xl md:text-4xl" />
-            <span className="text-xs md:text-sm mt-2">Pressure: {cityData.main.pressure} hPa</span>
-          </div>
+            <WiMoonAltNew className="text-blue-400 text-3xl md:text-4xl" />
+          <div className="text-xs md:text-sm mt-2" dangerouslySetInnerHTML={{ __html: getCurrentDateTime(cityData.timezone) }} />
+        </div>
         </div>
       </div>
     </div>
