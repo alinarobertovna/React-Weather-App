2e1016e499488a93e727bb8f0c8469c03aae6a2f
Alina <alinarobertovna17.03@gmail.com>
2024-01-08 02:15:16 -0500

    Changed layout, added awesome icons, added illustrations

--- src/components/CityWeather.js
+++ src/components/CityWeather.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { WiRain, WiStrongWind, WiHumidity, WiMoonAltNew, WiCloud } from 'react-icons/wi';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWind, faTemperatureHigh, faClock, faDroplet } from '@fortawesome/free-solid-svg-icons';
 import { BsSearch, BsGeoAlt, BsExclamationCircleFill } from 'react-icons/bs';
 
 const CityWeather = ({ cityData, handleSubmit, isCelsius, toggleTempUnit, error }) => {
@@ -30,7 +31,6 @@ const CityWeather = ({ cityData, handleSubmit, isCelsius, toggleTempUnit, error 
     // Formatting date and time
     const day = localTime.getDate();
     const monthIndex = localTime.getMonth();
-    const year = localTime.getFullYear();
     const hours = localTime.getHours();
     const minutes = localTime.getMinutes();
     const formattedHours = hours < 10 ? `0${hours}` : hours;
@@ -38,78 +38,97 @@ const CityWeather = ({ cityData, handleSubmit, isCelsius, toggleTempUnit, error 
     const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     const monthName = monthNames[monthIndex];
 
-    return `Date: ${day} ${monthName} ${year} Time: ${formattedHours}:${formattedMinutes}`;
+    return `${day} ${monthName} ${formattedHours}:${formattedMinutes}`;
+  };
+
+  const getWeatherImageUrl = (weatherCondition) => {
+    const weatherImages = {
+      'Rain': '/rainy.jpg',
+      'Clear': 'url-to-sunny-image.jpg',
+      'Clouds': 'url-to-cloudy-image.jpg',
+      'Snow': '/snow.jpg',
+      'Windy': '/windy.jpg'
+      // ... add more conditions and images as needed
+    };
+
+    return weatherImages[weatherCondition] || '/turbine.jpg';
   };
 
   return (
-    <div className="relative bg-white bg-opacity-80 rounded-lg shadow-lg overflow-hidden w-full p-4 lg:max-w-3xl lg:mx-auto">
+    <div className="mx-auto my-10 p-4 bg-white rounded-lg shadow-lg" style={{ width: '100%', maxWidth: '600px' }}>
       {/* Search bar row */}
-    <form onSubmit={handleFormSubmit} className="mb-4 w-full">
-      <div className="relative">
-      <input
-      type="text"
-      value={cityInput}
-      onChange={handleCityInputChange}
-      placeholder="Enter city"
-      className="pl-2 pr-10 py-2 border border-gray-300 rounded-lg w-full"
-      />
-      <button
-      type="submit"
-      className="absolute inset-y-0 right-0 bg-blue-500 text-white px-3 rounded-r-lg"
-      >
-      <BsSearch />
-      </button>
-    </div>
-  </form>
+      <form onSubmit={handleFormSubmit} className="mb-4 w-full">
+        <div className="relative">
+          <input
+            type="text"
+            value={cityInput}
+            onChange={handleCityInputChange}
+            placeholder="Enter city"
+            className="pl-2 pr-10 py-2 border border-gray-300 rounded-lg outline-0 w-full"
+          />
+          <button
+            type="submit"
+            className="absolute inset-y-0 right-0 bg-blue-500 text-white px-3 rounded-r-lg"
+          >
+            <BsSearch />
+          </button>
+        </div>
+      </form>
 
       {/* Display Error Message */}
-      {error && <div className="flex items-center justify-center"> <BsExclamationCircleFill className="mr-2" /> {error}</div>}
-      
+      {error && <div className="flex items-center justify-center"><BsExclamationCircleFill className="mr-2" /> {error}</div>}
+
       {/* Weather details, rendered only if cityData is available */}
       {cityData && !error && (
         <div className="text-gray-700">
-          {/* Location row with temperature unit toggle */}
-          <div className="flex justify-between items-center mb-4">
+          {/* Location and Temperature Unit Toggle */}
+          <div className="flex justify-between items-center flex-wrap sm:flex-nowrap mb-4">
             <div className="flex items-center">
-              <BsGeoAlt className="text-yellow-400 text-3xl mr-3" />
-              <span className="font-semibold text-lg md:text-2xl">{cityData.name}, {cityData.sys.country}</span> 
+              <BsGeoAlt className="text-black-400 text-3xl mr-3" />
+              <span className="font-semibold text-lg">{cityData.name}, {cityData.sys.country}</span>
             </div>
-            <button onClick={toggleTempUnit} className="text-gray-600 font-semibold text-lg md:text-2xl">
-              {isCelsius ? '°F' : '°C'}
-            </button>
+            <div className="flex items-center mt-2 sm:mt-0">
+              <button onClick={toggleTempUnit} className="text-gray-600 font-semibold text-lg">
+                {isCelsius ? '°F' : '°C'}
+              </button>
+            </div>
           </div>
 
           {/* Temperature & Date row */}
-          <div className="my-4 md:my-6 text-center">
-            <span className="text-gray-800 font-bold text-4xl md:text-6xl lg:text-9xl">{Math.round(cityData.main.temp)}</span>
-            <span className="text-gray-800 font-semibold text-xl md:text-2xl lg:text-4xl">{isCelsius ? '°C' : '°F'}</span>
-            <div className="text-gray-600 text-sm md:text-md lg:text-lg">
+          <div className="my-4 text-center">
+            <span className="text-gray-800 font-bold text-4xl">{Math.round(cityData.main.temp)}</span>
+            <span className="text-gray-800 font-semibold text-xl">{isCelsius ? '°C' : '°F'}</span>
+            <div className="text-gray-600 text-sm">
               Feels like: {Math.round(cityData.main.feels_like)}{isCelsius ? '°C' : '°F'}
             </div>
+            <div className="text-gray-600 text-sm">{cityData.weather[0].main}</div>
           </div>
 
+          {/* Weather Illustration */}
+          <img
+            src={getWeatherImageUrl(cityData.weather[0].main)}
+            alt="Weather Illustration"
+            className="mx-auto object-fill h-60 w-120"
+          />
+
           {/* Additional weather details row */}
-          <div className="grid grid-cols-2 md:grid-cols-6 gap-2">
+          <div className="grid grid-cols-2 gap-4">
             <div className="flex flex-col items-center">
-              <WiRain className="text-blue-400 text-3xl md:text-4xl" />
-              <span className="text-xs md:text-sm mt-2">High: {Math.round(cityData.main.temp_max)}°</span>
-              <span className="text-xs md:text-sm">Low: {Math.round(cityData.main.temp_min)}°</span>
+              <FontAwesomeIcon icon={faTemperatureHigh} size="lg" />
+              <span className="text-xs mt-2">High: {Math.round(cityData.main.temp_max)}°</span>
+              <span className="text-xs">Low: {Math.round(cityData.main.temp_min)}°</span>
             </div>
             <div className="flex flex-col items-center">
-              <WiStrongWind className="text-teal-400 text-3xl md:text-4xl" />
-              <span className="text-xs md:text-sm mt-2">Wind: {Math.round(cityData.wind.speed)} {isCelsius ? 'm/s' : 'mph'}</span>
+              <FontAwesomeIcon icon={faWind} size="lg" />
+              <span className="text-xs mt-2">Wind: {Math.round(cityData.wind.speed)} {isCelsius ? 'm/s' : 'mph'}</span>
             </div>
             <div className="flex flex-col items-center">
-              <WiHumidity className="text-blue-400 text-3xl md:text-4xl" />
-              <span className="text-xs md:text-sm mt-2">Humidity: {cityData.main.humidity}%</span>
+              <FontAwesomeIcon icon={faDroplet} size="lg" />
+              <span className="text-xs mt-2">Humidity: {cityData.main.humidity}%</span>
             </div>
             <div className="flex flex-col items-center">
-              <WiCloud className="text-gray-400 text-3xl md:text-4xl" />
-              <span className="text-xs md:text-sm mt-2">Visibility: {isCelsius ? Math.round(cityData.visibility / 1000) + ' km' : (Math.round(cityData.visibility * 0.000621371)).toFixed(1) + ' mi'}</span>
-            </div>
-            <div className="flex flex-col items-center">
-              <WiMoonAltNew className="text-blue-400 text-3xl md:text-4xl" />
-              <div className="text-xs md:text-sm mt-2">{getCurrentDateTime(cityData.timezone)}</div> 
+              <FontAwesomeIcon icon={faClock} size="lg" />
+              <span className="text-xs mt-2">{getCurrentDateTime(cityData.timezone)}</span>
             </div>
           </div>
         </div>
